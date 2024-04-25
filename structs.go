21183c571d1d17e0b3f261e0316ffb89e3ca3869
Maximilian Eschenbacher <Maximilian.Eschenbacher@tq-group.com>
2024-04-25 14:09:35 +0200

    File header updated
    Signed-off-by: Maximilian Eschenbacher <Maximilian.Eschenbacher@tq-group.com>

--- structs.go
+++ structs.go
@@ -1,3 +1,14 @@
+/*
+ * Generic Data Record - structs.go
+ * Copyright (c) 2018 - 2024 TQ-Systems GmbH <license@tq-group.com>, D-82229 Seefeld, Germany. All rights reserved.
+ * Author: Alexander Pögelt and the Energy Manager development team
+ *
+ * This software code contained herein is licensed under the terms and conditions of
+ * the TQ-Systems Product Software License Agreement Version 1.0.1 or any later version.
+ * You will find the corresponding license text in the LICENSE file.
+ * In case of any license issues please contact license@tq-group.com.
+ */
+
 package gdr
 
 // OBISCode for Data-Value:
