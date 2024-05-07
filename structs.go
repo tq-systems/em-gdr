49dccf495c647388c7d684ef24f2b626d9452f8d
Maximilian Eschenbacher <Maximilian.Eschenbacher@tq-group.com>
2024-05-07 08:49:27 +0200

    structs.go: OBIS indicator for negative power factor added
    BREAKING CHANGE:
    Renamed ObisIndicatorPowerFactor to ObisIndicatorPowerFactorPlus.
    
    Signed-off-by: Maximilian Eschenbacher <Maximilian.Eschenbacher@tq-group.com>

--- structs.go
+++ structs.go
@@ -29,16 +29,17 @@ type OBISCode struct {
 
 // Obis indicators and modes used in OBISCode
 const (
-	ObisIndicatorActivePlus    = 1
-	ObisIndicatorActiveMinus   = 2
-	ObisIndicatorReactivePlus  = 3
-	ObisIndicatorReactiveMinus = 4
-	ObisIndicatorApparentPlus  = 9
-	ObisIndicatorApparentMinus = 10
-	ObisIndicatorCurrent       = 11
-	ObisIndicatorVoltage       = 12
-	ObisIndicatorPowerFactor   = 13
-	ObisIndicatorFrequency     = 14
+	ObisIndicatorActivePlus       = 1
+	ObisIndicatorActiveMinus      = 2
+	ObisIndicatorReactivePlus     = 3
+	ObisIndicatorReactiveMinus    = 4
+	ObisIndicatorApparentPlus     = 9
+	ObisIndicatorApparentMinus    = 10
+	ObisIndicatorCurrent          = 11
+	ObisIndicatorVoltage          = 12
+	ObisIndicatorPowerFactorPlus  = 13
+	ObisIndicatorFrequency        = 14
+	ObisIndicatorPowerFactorMinus = 15
 
 	// ObisIndicatorPhaseModulo can be used to transform the indicators of phases L1-L3
 	// to the indicators specified above
