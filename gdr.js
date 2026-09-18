527ddbb47e3f9d947b1aa64d39305a1d0d029f7b
Ronny Freyer <ronny.freyer@tq-group.com>
2026-09-18 07:52:51 +0200

    Add device type "energy-management-system"
    Signed-off-by: Ronny Freyer <ronny.freyer@tq-group.com>

--- gdr.js
+++ gdr.js
@@ -1376,6 +1376,7 @@ $root.GCR = (function() {
             case 60:
             case 61:
             case 62:
+            case 63:
                 break;
             }
         if (message.meta != null && message.hasOwnProperty("meta")) {
@@ -1723,6 +1724,10 @@ $root.GCR = (function() {
         case "DEVICE_TYPE_CHARGING_GROUP":
         case 62:
             message.devicetype = 62;
+            break;
+        case "DEVICE_TYPE_ENERGY_MANAGEMENT_SYSTEM":
+        case 63:
+            message.devicetype = 63;
             break;
         }
         if (object.meta) {
@@ -2606,6 +2611,7 @@ $root.Unit = (function() {
  * @property {number} DEVICE_TYPE_DEVICE_SELF=60 DEVICE_TYPE_DEVICE_SELF value
  * @property {number} DEVICE_TYPE_GRID_CONNECTION_POINT_HUB=61 DEVICE_TYPE_GRID_CONNECTION_POINT_HUB value
  * @property {number} DEVICE_TYPE_CHARGING_GROUP=62 DEVICE_TYPE_CHARGING_GROUP value
+ * @property {number} DEVICE_TYPE_ENERGY_MANAGEMENT_SYSTEM=63 DEVICE_TYPE_ENERGY_MANAGEMENT_SYSTEM value
  */
 $root.DeviceType = (function() {
     var valuesById = {}, values = Object.create(valuesById);
@@ -2672,6 +2678,7 @@ $root.DeviceType = (function() {
     values[valuesById[60] = "DEVICE_TYPE_DEVICE_SELF"] = 60;
     values[valuesById[61] = "DEVICE_TYPE_GRID_CONNECTION_POINT_HUB"] = 61;
     values[valuesById[62] = "DEVICE_TYPE_CHARGING_GROUP"] = 62;
+    values[valuesById[63] = "DEVICE_TYPE_ENERGY_MANAGEMENT_SYSTEM"] = 63;
     return values;
 })();
 
